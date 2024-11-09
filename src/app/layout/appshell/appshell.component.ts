d37433f02c80ccf45ebc7a41aac8ed6cb3772c13
SvenvtH <sven@hoenderdaal.net>
2024-11-09 14:02:01 +0100

    .

--- src/app/layout/appshell/appshell.component.ts
+++ src/app/layout/appshell/appshell.component.ts
@@ -91,8 +91,16 @@ import { CommonModule } from '@angular/common';
               <nav class="mt-5 px-2 space-y-1">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a
-                  href="#"
-                  class="bg-gray-900 text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                  *ngFor="let item of navItems"
+                  [routerLink]="item.path"
+                  routerLinkActive="bg-gray-900 text-white"
+                  #rla="routerLinkActive"
+                  [routerLinkActiveOptions]="{ exact: item.exact ?? false }"
+                  [ngClass]="{
+                    'hover:text-white hover:bg-gray-700 text-gray-300':
+                      !rla.isActive
+                  }"
+                  class="group flex items-center px-2 py-2 text-base font-medium rounded-md"
                 >
                   <!--
               Heroicon name: outline/home
@@ -116,121 +124,6 @@ import { CommonModule } from '@angular/common';
                   </svg>
                   Dashboard
                 </a>
-
-                <a
-                  href="#"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                >
-                  <!-- Heroicon name: outline/users -->
-                  <svg
-                    class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
-                  Team
-                </a>
-
-                <a
-                  href="#"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                >
-                  <!-- Heroicon name: outline/folder -->
-                  <svg
-                    class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                    />
-                  </svg>
-                  Projects
-                </a>
-
-                <a
-                  href="#"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                >
-                  <!-- Heroicon name: outline/calendar -->
-                  <svg
-                    class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                    />
-                  </svg>
-                  Calendar
-                </a>
-
-                <a
-                  href="#"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                >
-                  <!-- Heroicon name: outline/inbox -->
-                  <svg
-                    class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                    />
-                  </svg>
-                  Documents
-                </a>
-
-                <a
-                  href="#"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                >
-                  <!-- Heroicon name: outline/chart-bar -->
-                  <svg
-                    class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Reports
-                </a>
               </nav>
             </div>
             <div class="flex-shrink-0 flex bg-gray-700 p-4">
@@ -244,7 +137,7 @@ import { CommonModule } from '@angular/common';
                     />
                   </div>
                   <div class="ml-3">
-                    <p class="text-base font-medium text-white">Tom Cook</p>
+                    <p class="text-base font-medium text-white">Red</p>
                     <p
                       class="text-sm font-medium text-gray-400 group-hover:text-gray-300"
                     >
@@ -316,7 +209,7 @@ import { CommonModule } from '@angular/common';
                     />
                   </div>
                   <div class="ml-3">
-                    <p class="text-sm font-medium text-white">Tom Cook</p>
+                    <p class="text-sm font-medium text-white">Red</p>
                     <p
                       class="text-xs font-medium text-gray-300 group-hover:text-gray-200"
                     >
